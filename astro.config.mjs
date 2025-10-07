49ceea59c7fcf3035c1e46515677ae154cca2570
b-nurmatov <p3rs0n.09@yandex.com>
2025-10-07 11:44:57 +0500

    Обновление конфига.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,23 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: { plugins: [tailwindcss()] },
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (chunkInfo) => {
+            if (chunkInfo.name && chunkInfo.name.endsWith(".css")) {
+              return "assets/styles[extname]"; // или просто 'styles.css' без хеша
+            }
+            return "assets/[name][extname]";
+          },
+        },
+      },
+    },
+  },
   server: {
     host: "192.168.0.220",
   },
+  compressHTML: false,
 });
